43cbdc2fbca475a09de24d6bd8907e20865bc618
Wenish <Wenish@users.noreply.github.com>
2022-09-25 01:34:08 +0200

    add chart

---
+++ src/components/BarChart.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <h2>What genre of multiplayer game are you developing?</h2>
+        <Bar 
+        :chart-options="{
+            responsive: true,
+            indexAxis: 'y',
+        }"
+            :chart-data="chartData"
+        />
+    </div>
+</template>
+<script lang="ts" setup>
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+const chartData = {
+      labels: [
+        'Shooter',
+        'MOBA',
+        'Platformer',
+        'RPG',
+        'Puzzlers and Party Game',
+        'Turn-based Game',
+        'Sandbox',
+        'Other'
+      ],
+      datasets: [
+        {
+          label: 'Data One',
+          backgroundColor: '#f87979',
+          data: [19, 17, 11, 33, 25, 38, 12, 8]
+        }
+      ]
+    }
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+</script>
--- src/pages/PageSurvey2022.vue
+++ src/pages/PageSurvey2022.vue
@@ -1,6 +1,8 @@
 <template>
     <LayoutBase>
         <h1>Results - 2022 September</h1>
+        <br />
+        <BarChart></BarChart>
     </LayoutBase>
     <div>
         
@@ -8,4 +10,5 @@
 </template>
 <script lang="ts" setup>
 import LayoutBase from '../layouts/LayoutBase.vue';
+import BarChart from '../components/BarChart.vue';
 </script>
