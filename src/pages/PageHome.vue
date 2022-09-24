33b38a45a2b22af542a121506f6d666ee7bac67e
Wenish <Wenish@users.noreply.github.com>
2022-09-24 20:57:18 +0200

    Update PageHome.vue

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,18 +1,15 @@
 <template>
     <LayoutBase>
-        <h1>Overview</h1>
+        <h1>Results</h1>
         <br />
-        <div class="list">
-            <div class="grid">
-                <router-link :to="'/survey/2022'">
-                    <div class="w-full h-full bg-violet-500 hover:bg-violet-400 p-8 grid rounded-sm">
-                        <div class="inline-grid">
-                            <span class="text-lg">Results</span>
-                            <span class="text-4xl font-bold">2022 Sept.</span>
-                        </div>
+        <div class="list grid gap-4">
+            <router-link :to="'/survey/2022'">
+                <div class="w-full h-full bg-violet-500 hover:bg-violet-400 p-8 grid rounded-sm">
+                    <div class="inline-grid">
+                        <span class="text-4xl font-bold">2022 Sept.</span>
                     </div>
-                </router-link>
-            </div>
+                </div>
+            </router-link>
         </div>
     </LayoutBase>
 </template>
@@ -21,7 +18,6 @@ import LayoutBase from '../layouts/LayoutBase.vue';
 </script>
 <style scoped>
 .list {
-    @apply grid;
     grid-template-columns: repeat(auto-fill, minmax(242px, 1fr));
 }
 </style>
