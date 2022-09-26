7e46af9a3892607bb3569bd52151942230615d87
Wenish <Wenish@users.noreply.github.com>
2022-09-26 21:09:16 +0200

    Create IconColyseus.vue

---
+++ src/components/IconColyseus.vue
@@ -0,0 +1,29 @@
+<template>
+    <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+        viewBox="0 0 65 65">
+        <defs>
+            <linearGradient id="New_Gradient_Swatch_1" x1="199.03" y1="82.41" x2="102.09" y2="-14.52"
+                gradientUnits="userSpaceOnUse">
+                <stop offset="0" stop-color="#4485ff" />
+                <stop offset="1" stop-color="#9b56ff" />
+            </linearGradient>
+            <linearGradient id="New_Gradient_Swatch_1-2" x1="59.25" y1="56.7" x2="11.34" y2="8.79"
+                xlink:href="#New_Gradient_Swatch_1" />
+        </defs>
+        <g id="Left_Aligned_-_Main" data-name="Left Aligned - Main">
+            <g id="Icon_-_Main" data-name="Icon - Main">
+                <path class="cls-2"
+                    d="M1.7,39.47c0,1.85,0,4,.06,6.6,0,5.15,7.92,14.5,52.16,16-.92-2.61-1.79-5.31-2.64-7.72C43.36,54.07,7,52.65,1.7,39.47ZM50.42,52c-.9-2.54-1.79-5.26-2.64-7.68C35,43.86,16.06,42,6.1,34.83a15,15,0,0,1-4.4-5c-.06,5.23-.19,7.81.72,8.88C3.48,40,9.54,50.43,50.42,52Zm-3.36-9.84c-.89-2.51-1.76-5.64-2.64-8.16-6-.32-38.67-3.08-42.73-14.79a.79.79,0,0,0-.09.26c0,2.51,0,5.06.1,7.57C1.7,27,2.58,39.85,47.06,42.11ZM52.82,8c.74-1.74,1.36-3.2,2.16-5C20.14,3.58,1,11.25,5.52,20,5.86,19.08,12.51,9.13,52.82,8ZM5.54,20l0-.05C5.49,20.05,5.5,20.07,5.54,20Zm45.84-8.88c-29.83.79-39.23,8.6-35.52,14.16,1.57-1.42,4.58-7.7,33.36-9.12ZM26.66,24.59a3.06,3.06,0,0,0,.24,4.56c5.6-4.33,12.19-4.79,19-5.52.75-1.76,1.47-3.18,2.28-5.07-11.14.79-14.66,1.89-18.6,3.87A7.77,7.77,0,0,0,26.66,24.59Z" />
+            </g>
+        </g>
+    </svg>
+</template>
+<style scoped>
+    .cls-1 {
+        fill: url(#New_Gradient_Swatch_1);
+    }
+
+    .cls-2 {
+        fill: url(#New_Gradient_Swatch_1-2);
+    }
+</style>
